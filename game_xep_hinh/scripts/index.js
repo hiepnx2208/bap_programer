d3290ca98d716119c87d69fa514feb1ceaf43e38
Hiep Nguyen Xuan <tung.vo@zaitenllc.com>
2023-06-03 07:15:55 +0700

    init message

---
+++ game_xep_hinh/scripts/index.js
@@ -0,0 +1,241 @@
+/*** CONSTANT***/
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+const COLOR_MAPPING = [
+    'red',
+    'orange',
+    'green',
+    'purple',
+    'blue',
+    'cyan',
+    'yellow',
+    'white',
+];
+
+const BRICK_LAYOUT = [
+    [
+      [
+        [1, 7, 7],
+        [1, 1, 1],
+        [7, 7, 7],
+      ],
+      [
+        [7, 1, 1],
+        [7, 1, 7],
+        [7, 1, 7],
+      ],
+      [
+        [7, 7, 7],
+        [1, 1, 1],
+        [7, 7, 1],
+      ],
+      [
+        [7, 1, 7],
+        [7, 1, 7],
+        [1, 1, 7],
+      ],
+    ],
+    [
+      [
+        [7, 1, 7],
+        [7, 1, 7],
+        [7, 1, 1],
+      ],
+      [
+        [7, 7, 7],
+        [1, 1, 1],
+        [1, 7, 7],
+      ],
+      [
+        [1, 1, 7],
+        [7, 1, 7],
+        [7, 1, 7],
+      ],
+      [
+        [7, 7, 1],
+        [1, 1, 1],
+        [7, 7, 7],
+      ],
+    ],
+    [
+      [
+        [1, 7, 7],
+        [1, 1, 7],
+        [7, 1, 7],
+      ],
+      [
+        [7, 1, 1],
+        [1, 1, 7],
+        [7, 7, 7],
+      ],
+      [
+        [7, 1, 7],
+        [7, 1, 1],
+        [7, 7, 1],
+      ],
+      [
+        [7, 7, 7],
+        [7, 1, 1],
+        [1, 1, 7],
+      ],
+    ],
+    [
+      [
+        [7, 1, 7],
+        [1, 1, 7],
+        [1, 7, 7],
+      ],
+      [
+        [1, 1, 7],
+        [7, 1, 1],
+        [7, 7, 7],
+      ],
+      [
+        [7, 7, 1],
+        [7, 1, 1],
+        [7, 1, 7],
+      ],
+      [
+        [7, 7, 7],
+        [1, 1, 7],
+        [7, 1, 1],
+      ],
+    ],
+    [
+      [
+        [7, 7, 7, 7],
+        [1, 1, 1, 1],
+        [7, 7, 7, 7],
+        [7, 7, 7, 7],
+      ],
+      [
+        [7, 7, 1, 7],
+        [7, 7, 1, 7],
+        [7, 7, 1, 7],
+        [7, 7, 1, 7],
+      ],
+      [
+        [7, 7, 7, 7],
+        [7, 7, 7, 7],
+        [1, 1, 1, 1],
+        [7, 7, 7, 7],
+      ],
+      [
+        [7, 1, 7, 7],
+        [7, 1, 7, 7],
+        [7, 1, 7, 7],
+        [7, 1, 7, 7],
+      ],
+    ],
+    [
+      [
+        [7, 7, 7, 7],
+        [7, 1, 1, 7],
+        [7, 1, 1, 7],
+        [7, 7, 7, 7],
+      ],
+      [
+        [7, 7, 7, 7],
+        [7, 1, 1, 7],
+        [7, 1, 1, 7],
+        [7, 7, 7, 7],
+      ],
+      [
+        [7, 7, 7, 7],
+        [7, 1, 1, 7],
+        [7, 1, 1, 7],
+        [7, 7, 7, 7],
+      ],
+      [
+        [7, 7, 7, 7],
+        [7, 1, 1, 7],
+        [7, 1, 1, 7],
+        [7, 7, 7, 7],
+      ],
+    ],
+    [
+      [
+        [7, 1, 7],
+        [1, 1, 1],
+        [7, 7, 7],
+      ],
+      [
+        [7, 1, 7],
+        [7, 1, 1],
+        [7, 1, 7],
+      ],
+      [
+        [7, 7, 7],
+        [1, 1, 1],
+        [7, 1, 7],
+      ],
+      [
+        [7, 1, 7],
+        [1, 1, 7],
+        [7, 1, 7],
+      ],
+    ],
+  ];
+
+const WHITE_COLOR_ID = 7;
+
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+class Board {
+    constructor(ctx) {
+     this.ctx = ctx;
+     this.grid = this.generateWhiteBoard();
+    }
+   
+  generateWhiteBoard() {
+    return Array.from({length: ROWS} , () => Array(COLS).fill(WHITE_COLOR_ID));
+  } 
+
+  drawCell(xAxis, yAxis, colorId) { // xAxis => 1 yAxis => 1
+    this.ctx.fillStyle = COLOR_MAPPING[colorId] || COLOR_MAPPING[WHITE_COLOR_ID];
+    this.ctx.fillRect(xAxis * BLOCK_SIZE, yAxis * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    this.ctx.fillStyle = 'black';
+    this.ctx.strokeRect(xAxis * BLOCK_SIZE, yAxis * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+  }
+
+  drawBoard() {
+    for (let row = 0; row < this.grid.length; row++) {
+        for (let col = 0; col < this.grid[0].length; col++) {
+          this.drawCell(col, row, WHITE_COLOR_ID );
+        }
+    }
+  }
+}
+
+class Brick{
+  constructor(id) {
+    this.id = id;
+    this.layout = BRICK_LAYOUT[id]
+    this.activeIndex = 0;
+    this.colPos = 3;
+    this.rowPos = 5;
+  }
+  
+  draw() {
+    for (let row = 0; row < this.layout[this.activeIndex].length; row++) {
+        for (let col = 0; col < this.layout[this.activeIndex][0].length; col++) {
+          if (this.layout[this.activeIndex][row][col] !== WHITE_COLOR_ID) {
+           board.drawCell(col + this.colPos, row + this.rowPos, this.id); 
+          } 
+        }
+    }
+  }
+}
+
+var board = new Board(ctx);
+board.drawBoard();
+brick = new Brick(0);
+brick.draw();
+//board.drawCell(1, 1, 1);
+
+console.table(board.grid);
